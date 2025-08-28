4217e6d5b54e7803500c43777a12cd23432d77f5
ALS <88404484+AugustoSLS@users.noreply.github.com>
2025-08-28 18:41:44 -0300

    logo iphone

--- js/iphonedetection.js
+++ js/iphonedetection.js
@@ -2,5 +2,5 @@ const isIphone = /iPhone/i.test(navigator.userAgent);
 
 if (isIphone) {
     document.getElementById("backgroundheadertext").classList.add("iphone");
-    document.getElementById("mun-ra").classList.add("iphone-logo");
+    document.getElementById("mun-ra").classList.add("iphonelogo");
 }
