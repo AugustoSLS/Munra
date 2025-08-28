aa8f6d3323ae8df885327c97c00583b38575e735
ALS <88404484+AugustoSLS@users.noreply.github.com>
2025-08-28 18:54:56 -0300

    i

--- js/iphonedetection.js
+++ js/iphonedetection.js
@@ -3,4 +3,5 @@ const isIphone = /iPhone/i.test(navigator.userAgent);
 if (isIphone) {
     document.getElementById("backgroundheadertext").classList.add("iphone");
     document.getElementById("mun-ra").classList.add("iphonelogo");
+    document.getElementById("btnNav").classList.add("iphonebtnmenu");
 }
